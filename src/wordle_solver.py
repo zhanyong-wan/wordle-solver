ac0964ac0637b2fcb4a6f94841c738aa6b98ed05
Zhanyong Wan <41527376+zhanyong-wan@users.noreply.github.com>
2022-04-29 23:01:32 -0700

    Add logic for the first guess.

--- src/wordle_solver.py
+++ src/wordle_solver.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 
 import os
+import random
 
 def GetWordList():
   py_file_dir = os.path.dirname(__file__)
@@ -10,8 +11,8 @@ def GetWordList():
   words = []
   with open(wordle_list_file, 'r') as f:
     for line in f.readlines():
-      word = line.strip().lower()
-      if word:
+      word = line.strip().upper()
+      if len(word) == 5:
         words.append(word)
   return words
 
@@ -22,11 +23,52 @@ def GetLetterFrequencies(words):
       letter_freq[letter] += 1
   return letter_freq
 
+def SortByLetterFrequencies(words):
+  letter_freq = GetLetterFrequencies(words)
+  word_weights = []
+  for word in words:
+    weight = sum(letter_freq[ch] for ch in set(word))
+    word_weights.append((word, weight))
+  return sorted(word_weights, key=lambda word_weight: word_weight[1], reverse=True)
+
+def GetHints(guess, answer):
+  hints = ''
+  for i, letter in enumerate(guess):
+    if letter == answer[i]:
+      hint = 'M'  # The letter and its position are correct.
+    elif letter in answer:
+      hint = 'O'  # The letter is in the answer, but in a different position.
+    else:
+      hint = 'X'  # The letter is not in the answer.
+    hints += hint
+  return hints
+
+def Colored(r, g, b, text):
+  return f'\033[38;2;{r};{g};{b}m{text}\033[38;2;255;255;255m'
+
+def PrintHints(guess, hints):
+  formatted = ''
+  for i, letter in enumerate(guess):
+    hint = hints[i]
+    if hint == 'X':
+      formatted += Colored(127, 127, 127, letter)
+    elif hint == 'O':
+      formatted += Colored(255, 255, 0, letter)
+    else:
+      formatted += Colored(0, 255, 0, letter)
+  return formatted
+
 def main():
   print('Welcome to Zhanyong Wan\'s Wordle Solver!')
+  random.seed()
   words = GetWordList()
-  letter_freq = GetLetterFrequencies(words)
-  print(f'{letter_freq}')
+  answer = words[random.randrange(0, len(words))]
+  sorted_word_weights = SortByLetterFrequencies(words)
+  guess = sorted_word_weights[0][0]
+  print(f'Guess #1: {guess}')
+  hints = GetHints(guess, answer)
+  print(f'Hint: {hints}')
+  print(PrintHints(guess, hints))
 
 if __name__ == '__main__':
   main()
